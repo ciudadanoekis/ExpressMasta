417b64bb84e32cf5e0a26f8e235b0536cfc73603
Guillermo Molina <ciudadanoekis@hotmail.com>
2018-12-17 15:34:15 +0100

    mas cambios

--- modelo/storeModel.js
+++ modelo/storeModel.js
@@ -15,31 +15,10 @@ exports.getById = (idStore, done)=>{
     })
 }
 
-exports.insert=({name, city, country, continent, description, address, phonenumber, email, facebook, soundcloud, instagram, www, slug}, done)=>{
+exports.insert=({name, city, country, continent, description, address, phonenumber, email, facebook, soundcloud, instagram, www, slug, longitude, latitude}, done)=>{
     console.log(address);
 
-    // Geocoding
-    // geocoder.geocode("Atlanta, GA", function ( err, data ) {
-    //     console.log(data)
-    //   });
-      
-
-    // var options = {
-    //     provider: 'google',
-    //     httpAdapter: 'https', 
-    //     apiKey: 'AIzaSyBil8XETYEYAWvv9WsT4potsdyZRQnpUt0',
-    //     formatter: null 
-    //   };
-    
-    //   var geocoder = geo(options);
-     
-     
-    // // Using callback
-    // geocoder.geocode("Atlanta, GA", function(err, res) {
-    //     console.log(res);
-    //   });
-
-    db.get().query('insert into stores values (null, ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', [name, city, country, continent, description, address, phonenumber, email, 0, 0, facebook, soundcloud, instagram, www, slug, 0], (err, res)=>{
+    db.get().query('insert into stores values (null, ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', [name, city, country, continent, description, address, phonenumber, email,facebook, soundcloud, instagram, www, slug, longitude, latitude, 0], (err, res)=>{
         if(err) return done(err, null);
         done(null, res);
     })
--- routes/api/store.js
+++ routes/api/store.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var geo = require('node-geocoder')
 // var router= require('express').Router()
 var storemodel = require('../../modelo/storeModel');
 
@@ -14,12 +15,47 @@ router.get('/:slugStore', (req, res)=>{
 })
 
 router.post('/', (req, res)=>{
+    console.log(req.body)
+
+     var options = {
+        provider: 'google',
+        httpAdapter: 'https', 
+        apiKey: 'AIzaSyCKeQbO7c-6t-bDKQZorL7VAD8fdsN4xpk',
+        formatter: null 
+      };
     
-    storemodel.insert(req.body, (err, result)=>{
+      var geocoder = geo(options);
+     
+     
+    // Using callback
+    geocoder.geocode(req.body.address, function(err, res) {
+        if(err) console.log('ERROR', err)
+        console.log(res);
+      });
+    
+   
+      storemodel.insert({
+          name: req.body.name,
+          city: req.body.city,
+          country: req.body.country,
+          continent: req.body.continent,
+          description: req.body.description,
+          address: req.body.address,
+          phonenumber: req.body.phonenumber,
+          email: req.body.email,
+          facebook: req.body.facebook,
+          soundcloud: req.body.soundcloud,
+          instagram: req.body.instagram,
+          www: req.body.www,
+          slug: req.body.slug,
+          longitude: req.body.longitude,
+          latitude: req.body.latitude
+      }, (err, result) =>{
         if(err) return res.json({error: err.message})
         storemodel.getById(result.insertId,(err, rows)=>{
             res.json(rows[0]);
+            })
         })
     })
-})
+
 module.exports = router;
